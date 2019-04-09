f89267dbe66c61c4fffa04fbbc3e8d9f393b8ded
Davenchy <firon1222@gmail.com>
2019-04-09 19:22:10 +0200

    add visual effects and edit some styles

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 window.addEventListener('load', () => {
 
+  const effects = document.querySelector('.effects');
   const pads = document.querySelectorAll('.pads > div');
   const sounds = document.querySelectorAll('.pads audio');
+
+  const addEffects = color => {
+    const effect = document.createElement('div');
+    effect.className = "effect";
+    effect.style.background = color;
+    effect.addEventListener('animationend', () => effect.remove());
+    effects.appendChild(effect);
+  };
 
   pads.forEach((pad, index) => {
     pad.addEventListener('click', () => {
       const audio = sounds[index];
       audio.currentTime = 0;
       audio.play();
+      const color = window.getComputedStyle(pad)['background-color'];
+      addEffects(color);
     });
   });
 
